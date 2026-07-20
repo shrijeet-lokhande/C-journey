e79ed02277a6137651ab681987d1ed6a387962ad
shrijeet-lokhande <1820hitler@gmail.com>
2026-07-21 00:16:03 +0530

    created day6

---
+++ day6/for-loop.c
@@ -0,0 +1,19 @@
+// //this is a program to print the hello world for 5 times
+// #include<stdio.h>
+// int main(){
+    
+//     for(int i=0; i<5; i++){
+//         printf("hello world\n");
+//     }
+
+//     return 0;
+// }
+
+// following is the program to print numbers from 1 to 100
+
+#include<stdio.h>
+int main(){
+    for(int i=1; i<=100; i++ ){printf("%d\t", i);}
+
+    return 0;
+}
