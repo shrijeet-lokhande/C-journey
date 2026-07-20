ab0c4288b62536f1344768a9b7269b7c5ec99870
shrijeet-lokhande <1820hitler@gmail.com>
2026-07-20 19:52:59 +0530

    created day6

---
+++ day5/case-checker.c
@@ -0,0 +1,14 @@
+// this is a program to check if the letters are upper case or not
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter the character : ");
+    scanf("%c", &ch);
+
+    if(ch>='A' && ch<='Z'){
+        printf("\nUPPER CASE");
+    }
+    else{printf("\nlower case");}
+
+    return 0;
+}
